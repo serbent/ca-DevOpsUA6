ca95d485df0798dbd08d583c37e5d200761f7e47
serbent <andrius.solovej@gmail.com>
2026-01-13 20:53:03 +0200

    fixed upload. Uniq project name

--- 33/setup.py
+++ 33/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name='mocking',
+    name='serbentas-mocking',
     version='0.1',
     packages=find_packages(),
     install_requires=['requests']
