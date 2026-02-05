c9c22052da4a91de17e62c05b3ff5b18e570c6ae
serbent <andrius.solovej@gmail.com>
2026-02-05 21:13:36 +0200

    44

---
+++ 44/README.md
@@ -0,0 +1,1 @@
+# 44. API and postman.
---
+++ 44/curl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GITHUB_TOKEN=""
+
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/user/repos
